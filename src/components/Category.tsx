ed7e58e0f721e49790e69fd9e6ae97b63fd3cf14
dev-subhadeep <subhadeepsarkar1995@gmail.com>
2024-01-12 02:24:28 +0530

    added footer

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import CategoryCard from "./CategoryCard"
-import Living from "@/assets/image 100.png"
-import Dining from "@/assets/image 106.png"
+import Dining from "@/assets/image 100.png"
+import Living from "@/assets/image 106.png"
 import Bedroom from "@/assets/image 101.png"
 
 const Category = () => {
@@ -10,9 +10,9 @@ const Category = () => {
       <h2>Browse The Range</h2>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
       <div className="flex flex-row gap-4">
-        <CategoryCard key={1} image={Living} category="Living" />
+        <CategoryCard key={1} image={Living} category="Dining" />
         <CategoryCard key={2} image={Dining} category="Living" />
-        <CategoryCard key={3} image={Bedroom} category="Living" />
+        <CategoryCard key={3} image={Bedroom} category="Bedroom" />
       </div>
     </div>
   )
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,37 @@
 import React from "react"
 
 const Footer = () => {
-  return <div>Footer</div>
+  return <footer className="px-10 py-8 container mx-auto">
+    <div className="flex flex-row justify-between">
+    <div>
+      <h3 className="mb-[50px]">Furniro</h3>
+      <p>400 University Drive, Suite 200 Canal</p>
+      <p>Fl 33134 USA</p>
+    </div>
+    <div className="flex flex-row justify-between">
+      <ul>
+        <li>Links</li>
+        <li>Home</li>
+        <li>Shop</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+      <ul>
+        <li>Help</li>
+        <li>Payment Options</li>
+        <li>Returns</li>
+        <li>Privacy Policies</li>
+      </ul>
+    </div>
+    <div>
+      <h3>Newsletter</h3>
+      <input className="capitalize" placeholder="Enter your email address" /> <button className="uppercase border border-b-black">Subscribe</button>
+    </div>
+  </div>
+  <div className="border border-l-white border-r-white border-b-white border-t-slate-400 py-8">
+    <p>2023 furniro. All rights reserved </p>
+  </div>
+  </footer>
 }
 
 export default Footer
