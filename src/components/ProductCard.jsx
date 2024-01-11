d039a1dc2f7672b3b68c13f4a985b9e7c9693d94
dev-subhadeep <subhadeepsarkar1995@gmail.com>
2024-01-12 01:17:06 +0530

    added ProductCard

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,12 +1,22 @@
 import React from "react"
+import SampleImage from "@/assets/image 76.png"
+import Image from "next/image"
 
 const ProductCard = () => {
   return (
     <div>
-      <div className="relative">
-        <img src="" alt="" className="absolute" />
-        <div className="absolute">Leviosa</div>
-        <div className="absolute">New</div>
+      <div className="relative min-w-[300px] min-h-[400px]">
+        <Image src={SampleImage} alt="Furniture" fill className="absolute" />
+        <div className="absolute bg-gray-200 bottom-0 right-0 left-0 p-4 ">
+
+          <h3 className="text-lg font-bold">Slytherine</h3>
+          <p>Stylish Cafe Chair</p>
+          <p><b>Rp 20002.00p </b> <span className="line-through">Rp 200200.000</span></p>
+        </div>
+        <div className="invisible hover:visible bg-slate-black/80 absolute top-0 left-0 botton-0 right-0 h-full w-full flex flex-col justify-center items-center" >
+          <button className="border border-red-200 py-4 px-8">Add to cart</button>
+        </div>
+        <div className="absolute flex flex-col justify-center items-center text-white top-4 right-4 p-8 rounded-full bg-green-400 w-4 h-4">New</div>
       </div>
     </div>
   )
