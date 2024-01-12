ce7d32686334af5b5d86d0aa1274fe0c73cdbfa3
dev-subhadeep <subhadeepsarkar1995@gmail.com>
2024-01-12 19:37:38 +0530

    updated the Hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import Category from "@/components/Category"
-import Image from "next/image"
-import ProductCard from "@/components/ProductCard"
+import Category from "@/components/Category";
+import Image from "next/image";
+import ProductCard from "@/components/ProductCard";
+import Hero from "@/components/Hero";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Category />
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <Hero /> <Category />
       <ProductCard />
     </main>
-  )
+  );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,27 @@
-import React from "react"
-import HeroImage from "@/assets/hero.jpg"
-import Image from "next/image"
+import React from "react";
+import HeroImage from "@/assets/hero.jpg";
+import Image from "next/image";
+import Link from "next/link";
 
 const Hero = () => {
   return (
-    <div>
-      <Image src={HeroImage} fill alt="Furniro Hero Image" />
-      <div>Hero Caption</div>
+    <div className="w-full bg-gradient-to-r from-cyan-200 to-blue-500 flex flex-row justify-end">
+      <div className="px-6 py-10 flex flex-col gap-3 bg-yellow-50 w-[500px] mx-[50px] my-[100px]">
+        <h2 className="font-bold">New Arrival</h2>
+        <h1 className="text-4xl text-yellow-600 font-bold">
+          Discover Our <br />
+          New Collection
+        </h1>
+        <p>Lorem ipsum dolor sit amet unga boonga tellus nec ullalala natt.</p>
+        <Link
+          href={"/shop"}
+          className="uppercase bg-yellow-900 w-[200px] h-[60px] flex flex-row justify-center items-center text-white font-bold"
+        >
+          Buy now
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
