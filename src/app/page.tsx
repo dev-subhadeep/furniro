f8b3de37663a36fde0c5eb47f9e3429769074eae
dev-subhadeep <subhadeepsarkar1995@gmail.com>
2024-01-13 16:06:03 +0530

    added Inpiration section file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import Category from "@/components/Category";
 import Products from "@/components/Products";
 import Hero from "@/components/Hero";
+import Inspiration from "@/components/Inspiration";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Hero />
       <Category />
       <Products />
+      <Inspiration />
     </main>
   );
 }
---
+++ src/components/Inspiration.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import SampleImage from "@/assets/image 101.png";
+
+const Inspiration = () => {
+  return (
+    <div>
+      <div>
+        <h1 className="text-4xl font-bold">
+          50+ Beautiful rooms <br /> inspiration
+        </h1>
+        <p>
+          Our designer already made a lot of beautiful <br /> prototype of rooms
+          that inspire you.
+        </p>
+        <button className="bg-yellow-700 px-8 py-2 my-4 text-white font-bold">
+          Explore More
+        </button>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Inspiration;
